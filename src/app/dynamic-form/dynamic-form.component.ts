f94d838b60b8deba5de4dcf8bd820d7625d52182
andy1729 <ujjawalanand1729@gmail.com>
2020-08-31 08:01:45 +0000

    added form-field service and other things

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+/*
+  presents field types defined in model as form fields
+*/
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FormFieldBase } from './form-field-base'
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.css']
 })
-export class DynamicFormComponent implements OnInit {
+export class DynamicFormComponent  {
+  @Input() field: FormFieldBase<string>;
+  @Input() form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isValid() {
+    return this.form.controls[this.field.key].valid;
   }
 
 }
---
+++ src/app/dynamic-form/form-field.service.ts
@@ -0,0 +1,205 @@
+/*
+    Supplies query form fields in the form of an array 
+    bound to @Input fields
+*/
+
+import { Injectable } from '@angular/core';
+
+import { DropdownField } from './from-field-dropdown';
+import { FormFieldBase } from './form-field-base';
+import { TextboxField } from './form-field-textbox';
+import { of } from 'rxjs'
+
+@Injectable() 
+export class FOrmFieldService {
+    getFormFields() {
+        const BooleanFormFieldOption = [
+            {key: '', value:''},
+            {key: 'True', value: 'true'},
+            {key: 'False', value: 'false'}
+        ]
+    
+        const fields: FormFieldBase<string>[] = [
+            
+            new TextboxField( {
+                key: 'title',
+                label: 'Title',
+                value: '',
+                required: false,
+                order: 1
+            }),
+
+            new TextboxField( {
+                key: 'body',
+                label: 'Body',
+                value: '',
+                required: false,
+                order: 2
+            }),
+
+            new TextboxField( {
+                key: 'q',
+                label: 'Q',
+                value: '',
+                required: false,
+                order: 3
+            }),
+
+            new DropdownField({
+                key: 'sort',
+                label: 'Sort',
+                options: [
+                    {key: 'Activity', value: 'activity'},
+                    {key: 'Votes', value: 'votes'},
+                    {key: 'Creation', value: 'creation'},
+                    {key: 'Relevance', value: 'relevance'}
+                ],
+                order: 4
+            }),
+
+            new DropdownField({
+                key: 'order',
+                label: 'Order',
+                options: [
+                    {key: 'Ascending', value: 'asc'},
+                    {key: 'Descending', value: 'dsc'}
+
+                ],
+                order: 5
+            }),
+
+            new DropdownField({
+                key: 'closed',
+                label: 'Closed',
+                options: BooleanFormFieldOption
+                ,
+                order: 6
+            }),
+
+            new DropdownField({
+                key: 'accepted',
+                label: 'Accepted',
+                options: BooleanFormFieldOption,
+                order: 7
+            }),
+
+            new TextboxField( {
+                key: 'fromdate',
+                label: 'FromDate',
+                value: '',
+                required: false,
+                order: 8
+            }),
+
+            new TextboxField( {
+                key: 'views',
+                label: 'Views',
+                value: '',
+                required: false,
+                order: 9
+            }),
+
+            new TextboxField( {
+                key: 'answers',
+                label: 'Answers',
+                value: '',
+                required: false,
+                order: 10
+            }),
+
+            new TextboxField( {
+                key: 'todate',
+                label: 'Todate',
+                value: '',
+                required: false,
+                order: 11
+            }),
+
+            new TextboxField( {
+                key: 'notagged',
+                label: 'Notagged',
+                value: '',
+                required: false,
+                order: 12
+            }),
+
+            new TextboxField( {
+                key: 'tagged',
+                label: 'Tagged',
+                value: '',
+                required: false,
+                order: 13
+            }),
+
+            new TextboxField( {
+                key: 'min',
+                label: 'Min',
+                value: '',
+                required: false,
+                order: 14
+            }),
+
+            new TextboxField( {
+                key: 'max',
+                label: 'Max',
+                value: '',
+                required: false,
+                order: 15
+            }),
+
+            new DropdownField({
+                key: 'migrated',
+                label: 'Migrated',
+                options: BooleanFormFieldOption,
+                order: 16
+            }),
+
+            new DropdownField({
+                key: 'notice',
+                label: 'Notice',
+                options: BooleanFormFieldOption,
+                order: 17
+            }),
+
+            new TextboxField( {
+                key: 'user',
+                label: 'User',
+                value: '',
+                required: false,
+                order: 18
+            }),
+
+            new TextboxField( {
+                key: 'url',
+                label: 'Url',
+                value: '',
+                required: false,
+                order: 19
+            }),
+
+            new DropdownField({
+                key: 'wiki',
+                label: 'Wiki',
+                options: BooleanFormFieldOption,
+                order: 20
+            }),
+
+            new TextboxField( {
+                key: 'page',
+                label: 'Page',
+                value: '',
+                required: false,
+                order: 21
+            }),
+
+            new TextboxField( {
+                key: 'pagesize',
+                label: 'PageSize',
+                value: '',
+                required: false,
+                order: 22
+            })  
+        ];
+        return of(fields.sort((a, b) => a.order - b.order));
+    }
+}
