6278e9f2c7b3b8d4d8d3b3909412a28bc417181f
andy1729 <ujjawalanand1729@gmail.com>
2020-09-01 14:31:15 +0000

    added filters and hide tags

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -4,20 +4,28 @@ import { HttpClient } from '@angular/common/http'
 
 import { FormFieldBase } from './form-field-base';
 import { FormControlService } from './form-control.service';
+import { PageService } from './page.service';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styles: ['./dynamic-form.component.css'],
-  providers: [ FormControlService ]
+  providers: [ FormControlService, PageService ]
 })
 
 export class DynamicFormComponent implements OnInit {
   @Input() fields: FormFieldBase<string>[] = [];
   form: FormGroup;
-  payload = []
+  payload: any[]
 
-  constructor(private fcs: FormControlService, private http: HttpClient) { }
+  // pager object
+  pager: any = {};
+  //paged items
+  pagedItems: any[];
+
+  constructor(private fcs: FormControlService, 
+              private http: HttpClient,
+              private pageService: PageService) { }
 
   ngOnInit() {
     this.form = this.fcs.toFormGroup(this.fields);
@@ -29,12 +37,16 @@ export class DynamicFormComponent implements OnInit {
     formData['site'] = 'stackoverflow'
 
     this.http.get('https://api.stackexchange.com/2.2/search/advanced?'+this.dictToURI(formData))
-              .subscribe( (response) => this.payload =  response["items"],
+              .subscribe( (response) => 
+                {
+                  this.payload =  response["items"]
+                  this.setPage(1);
+                },
               (error) => console.log(error)
     )
   }
 
-  dictToURI(dict) {
+  dictToURI(dict: any = {}) {
     // convert dict into get request parameter
     var str = [];
     for(var p in dict){
@@ -42,5 +54,19 @@ export class DynamicFormComponent implements OnInit {
        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(dict[p]));
     }
     return str.join("&");
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    // get pager object from service
+    this.pager = this.pageService
+                      .getPager(this.payload.length, page);
+    
+    // get current page of items
+    this.pagedItems = this.payload.slice(this.pager.startIndex,
+                                          this.pager.endIndex+1) 
   }
 }
