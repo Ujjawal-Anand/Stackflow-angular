0fcff2fbfaae36c6086b4349175b9b8e77c96d9d
andy1729 <ujjawalanand1729@gmail.com>
2020-09-02 06:16:27 +0000

    completed the project

--- src/app/dynamic-form/form-field-numberbox.ts
+++
@@ -1,6 +0,0 @@
-/* class to be used to render a textbox input form field */
-import { FormFieldBase } from './form-field-base';
-
-export class TextboxField extends FormFieldBase<number> {
-    controlType = 'textbox'
-}
